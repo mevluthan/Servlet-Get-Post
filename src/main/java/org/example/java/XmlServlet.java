0bb92e9bcc683a5f98aec02d36b1c2a29067381a
mevluthan <58922055+mevluthan@users.noreply.github.com>
2021-05-15 09:51:14 +0300

    Servlet Get v Post Method

---
+++ src/main/java/org/example/java/XmlServlet.java
@@ -0,0 +1,26 @@
+package org.example.java;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XmlServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName=request.getParameter("userName");
+		out.println("Hello from the GET method! "+userName);
+	}
+	
+		protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName=request.getParameter("userName");
+		out.println("Hello from the POST method! "+userName);
+	}		
+}
